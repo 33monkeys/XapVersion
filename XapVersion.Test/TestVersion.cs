7ff80beb0dd8f6337b0d7d304f54a1d6561de42b
a.lisnyi <a.lisnyi@netrika.ru>
2015-01-16 15:13:01 +0300

    Test solution

---
+++ XapVersion.Test/TestVersion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace XapVersion.Test
+{
+    [TestFixture]
+    public class TestVersion
+    {
+        [TestCase]
+        [Repeat(100)]
+        public void GetVersion_Test()
+        {
+            foreach (var xap in XapFilesForTest)
+            {
+                var path = Path.Combine("ClientBin", xap.FileName);
+                var version = VersionOf.XapFile(path);
+
+                Assert.That(version, Is.EqualTo(xap.Version));
+            }
+        }
+
+        public List<Xap> XapFilesForTest = new List<Xap>
+        {};
+    }
+}
---
+++ XapVersion.Test/Xap.cs
@@ -0,0 +1,9 @@
+﻿namespace XapVersion.Test
+{
+    public class Xap
+    {
+        public string FileName { get; set; }
+
+        public string Version { get; set; }
+    }
+}
